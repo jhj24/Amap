666b3f1202d98289acb85ab95e33b6d1d5903381
jhj <727024586@qq.com>
2018-08-22 17:52:36 +0800

    Initial commit

---
+++ app/src/main/java/com/jhj/amapdemo/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.jhj.amapdemo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.amap.api.location.AMapLocation
+import com.jhj.locationlibrary.LocationCallback
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val location = (application as MyApplication).location
+
+        btn_location.setOnClickListener {
+            location?.setLocationListener(object : LocationCallback {
+                override fun onLocation(location: AMapLocation) {
+                    val address = location.address
+                    Toast.makeText(this@MainActivity, address, Toast.LENGTH_SHORT).show()
+                }
+            })
+            location.start()
+        }
+
+    }
+}
---
+++ app/src/main/java/com/jhj/amapdemo/MyApplication.java
@@ -0,0 +1,21 @@
+package com.jhj.amapdemo;
+
+import android.app.Application;
+
+import com.jhj.locationlibrary.Location;
+
+/**
+ * Created by jhj on 18-8-22.
+ */
+
+public class MyApplication extends Application {
+
+    public Location location;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        location = new Location(this);
+
+    }
+}
